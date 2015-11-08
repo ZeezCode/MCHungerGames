c2ac1321e80565f9f9d69ed4136602a5bcd5d813
Zee <amurphey7@gmail.com>
2015-11-07 16:37:23 -0800

    Updated getCanStart, not done

--- src/che/mchg/MCHGUtils.java
+++ src/che/mchg/MCHGUtils.java
@@ -1,12 +1,23 @@
 package che.mchg;
+
+import org.bukkit.ChatColor;
+
 public class MCHGUtils {
-	static Main plugin;
-	static int min;
+	private Main plugin;
+	private int min;
 	MCHGUtils(Main pl, int minP) {
 		this.plugin = pl;
 		this.min = minP;
 	}
+	public String getHGPrefix() {
+		return ChatColor.BLACK+"["+ChatColor.DARK_RED+"CHEHG"+ChatColor.BLACK+"] "+ChatColor.RESET;
+	}
 	public void checkCanBegin() {
-		//if (this.plugin.)
+		if (!(this.plugin.getServer().getOnlinePlayers().size() >= this.min)) {
+			startCountdown();
+		}
+	}
+	public void startCountdown() {
+		
 	}
 }
--- src/che/mchg/Main.java
+++ src/che/mchg/Main.java
@@ -2,6 +2,8 @@ package che.mchg;
 import java.util.ArrayList;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
