5b680f9bf443218a15f77e6a68e03d0bb4f74677
Zee <amurphey7@gmail.com>
2015-11-07 16:20:02 -0800

    Added initial files

---
+++ src/che/mchg/MCHGUtils.java
@@ -0,0 +1,12 @@
+package che.mchg;
+public class MCHGUtils {
+	static Main plugin;
+	static int min;
+	MCHGUtils(Main pl, int minP) {
+		this.plugin = pl;
+		this.min = minP;
+	}
+	public void checkCanBegin() {
+		//if (this.plugin.)
+	}
+}
---
+++ src/che/mchg/Main.java
@@ -0,0 +1,42 @@
+package che.mchg;
+import java.util.ArrayList;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.java.JavaPlugin;
+public class Main extends JavaPlugin implements Listener {
+	static MCHGUtils util;
+	static ArrayList<Player> players = new ArrayList<Player>();
+	public void onEnable() {
+		PluginDescriptionFile pdfFile = getDescription();
+		getLogger().info(pdfFile.getName()+" has been enabled running "+pdfFile.getVersion()+".");
+		Bukkit.getServer().getPluginManager().registerEvents(this, this);
+		saveDefaultConfig();
+		util =  new MCHGUtils(this, getConfig().getInt("minPlayers"));
+	}
+	@EventHandler
+	public void onPlayerConnect(PlayerJoinEvent e) {
+		Player ply = e.getPlayer();
+		if (Bukkit.getServer().getOnlinePlayers().size() >= getConfig().getInt("maxPlayers")) {
+			ply.kickPlayer("This server has reached the maximum amount of players allowed.\nPlease join back later!");
+			return;
+		}
+		players.add(ply);
+		String msg = ChatColor.BLACK+"["+ChatColor.DARK_RED+"CHEHG"+ChatColor.BLACK+"]"+ChatColor.YELLOW+ply.getName()+ChatColor.WHITE+" has joined the game!";
+		e.setJoinMessage(msg);
+		util.checkCanBegin();
+	}
+	@EventHandler
+	public void onPlayerLeave(PlayerQuitEvent e) {
+		Player ply = e.getPlayer();
+		if (players.contains(ply))
+			players.remove(ply);
+		String msg = ChatColor.BLACK+"["+ChatColor.DARK_RED+"CHEHG"+ChatColor.BLACK+"]"+ChatColor.YELLOW+ply.getName()+ChatColor.WHITE+" has left the game.";
+		e.setQuitMessage(msg);
+	}
+}
